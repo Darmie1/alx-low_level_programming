6cd297a4188dd1abbec7b5c90b541eafa438838b
Darmie1 <damzee63@gmail.com>
2023-03-03 22:17:42 -0800

    print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet - printing alphabet ten times
+ * return: void
+ */
+void print_alphabet_x10(void)
+{
+	char let;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (let = 'a'; let <= 'z'; let++)
+
+		_putchar(let);
+
+		_putchar('\n');
+		i++;
+	}
+}
