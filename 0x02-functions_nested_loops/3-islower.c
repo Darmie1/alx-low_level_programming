b140f3c4c8cba00175a6410158103dfae32c42da
Darmie1 <damzee63@gmail.com>
2023-03-03 23:05:02 -0800

    compare

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * int _islower - check for lower case'
+ * _islower - check for lower case'
  * @c: check a charater
  * Return: 1 for lowercase and 0 for anything else
  */
