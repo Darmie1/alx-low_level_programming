a57fa3a975f7dfbf57890670133089a7f38327b2
Darmie1 <damzee63@gmail.com>
2023-03-11 15:36:48 -0800

    mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * mul - write a function that multiokies two integers
+ * mul - write a function that multiplies two integers
  * @a:parameter to be checked
- * @b:parameter 2 ro be checked
+ * @b:parameter 2 to be cheked
  * Return: 0
  */
 int mul(int a, int b)
@@ -10,6 +10,6 @@ int mul(int a, int b)
 	int c;
 
 	c = a * b;
-	_putchar(c);
-	return (0);
+
+	return (c);
 }
