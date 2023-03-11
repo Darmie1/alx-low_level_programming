3389e775817015219341a28cbe79f546049f3dea
Darmie1 <damzee63@gmail.com>
2023-03-10 19:14:14 -0800

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,15 +2,15 @@
 /**
  * _isalpha - checking for alphabetic character
  * @c: check for alpha
- * Return: return 1 if c is a letter, lowercase or uppe
+ * Return: return 1 if c is a letter, lowercase or
+ * uppercase
  */
 
 int _isalpha(int c)
 
 {
-	for ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-	{
-	return (1);
-	}
-	return (0);
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
 }
