46f26200b1c6e31813bf5e899d3b65380d40accd
Darmie1 <damzee63@gmail.com>
2023-06-23 18:38:37 -0700

    print number

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - this func prints numbers
+ * @n: n is the number of interger passed into the func
+ * @separator: separatoe is the string to be printed
+ * Return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list mylist;
+	int nu;
+	unsigned int o;
+
+	va_start(mylist, n);
+
+	for (o = 0; o < n; o++)
+	{
+		nu = va_arg(mylist, int);
+		printf("%d", nu);
+	}
+	if (separator == NULL)
+	{
+		printf("NULL");
+	}
+	va_end(mylist);
+
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,7 @@
 #define VARIADIC_FUNCTIONS_H
 
 #include <stdarg.h>
+#include <stdio.h>
 /**
  * Note that stdarg.h is a standard header file in C that provides facilities for working with functions that accept a variable number of arguments. It defines a set of macros and types that allow you to access the individual arguments passed to such functions.
  */
